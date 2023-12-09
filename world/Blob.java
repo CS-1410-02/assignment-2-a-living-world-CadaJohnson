760811f42a9d0e59abe80e673b9e6dfbd482aa4d
CadaJohnson <141864342+CadaJohnson@users.noreply.github.com>
2023-12-08 20:03:55 -0700

    Update Blob.java

--- world/Blob.java
+++ world/Blob.java
@@ -4,8 +4,30 @@ import java.util.Random;
 public class Blob{
     public String Name; 
     Random r = new Random();
+    int determination = r.nextInt(3, 9);
+    
 
     public void SetName(String name){
         Name = name;
+    }
+
+    public boolean findFood(){
+        boolean[][] foodLocation = 
+        {{false, false, false},
+        {false, false, false},
+        {false, false, false}};
+
+        foodLocation[r.nextInt(0,3)][r.nextInt(0,3)] = true;
+
+        for (int i = 0; i < determination; i++)    
+        {
+            if (foodLocation[r.nextInt(0,3)][r.nextInt(0,3)] = true)
+            {
+                return true;
+            }
+        }
+        return false;
+        //if it gets it, yay! hunger goes down
+        //if not, the blob goes hungry :(
     }
 }
